3b9f52362b5dcf2c7729bf11691ddefd3ee7f1d2
Danu man <zillahypss@gmail.com>
2026-07-15 20:25:25 +0500

    worked on the js and html files a little

---
+++ Dani-APOD-web/src/main.js
@@ -0,0 +1,22 @@
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+document.querySelector("#app").innerHTML = "<p>Loading...</p>";
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+.then(Response => Response.json())
+.then(data => {
+    let media;
+    if (data.media_type === "image") {
+        media = `<img src="${data.url}" style="width: 300px; height: 200px;"  />`;
+    } else {
+        media = `<video src="${data.url}" controls></video>`;
+    }
+    document.querySelector("#app").innerHTML = `
+        <h1>${data.title}</h1>
+        <img src="${data.url}" />
+        <p>${data.explanation}</p>
+    `;
+})
+.catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`
+});
